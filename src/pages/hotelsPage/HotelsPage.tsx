957cc2ba06078038fbd5dd03a1ac5339cf373f49
Max Teclav <maks.tetslav@yandex.ru>
2022-04-08 01:05:47 +0300

    feat: add header

--- src/pages/hotelsPage/HotelsPage.tsx
+++ src/pages/hotelsPage/HotelsPage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from '@components/common/header/Header';
 import cl from './HotelsPage.module.scss';
 
 const HotelsPage: React.FC = () => {
-  return <main className={cl.container}></main>;
+  return (
+    <main className={cl.container}>
+      <Header />
+    </main>
+  );
 };
 
 export default HotelsPage;
