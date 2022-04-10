c0704de5214c77a062c31bbd4298f73bf3a7169a
Max Teclav <maks.tetslav@yandex.ru>
2022-04-10 18:49:09 +0300

    deploy: v0.1

--- src/models/auth.ts
+++
@@ -1,9 +0,0 @@
-export interface IAuthData {
-  email: string;
-  password: string;
-}
-
-export enum EAuthInputTypes {
-  EMAIL = 'email',
-  PASSWORD = 'password',
-}
--- src/utils/helpers/getEndDate.ts
+++
@@ -1,8 +0,0 @@
-const getEndDate = (startDate: string, days: string) => {
-  const date = new Date(startDate);
-  const endDate = date.setDate(date.getDate() + Number(days));
-
-  return new Date(endDate).toLocaleDateString().split('.').reverse().join('-');
-};
-
-export default getEndDate;
