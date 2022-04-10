cbac9acb11f145132e24d0a73f50acea6533b80b
Max Teclav <maks.tetslav@yandex.ru>
2022-04-10 17:54:23 +0300

    feat: implement hotelsContent component

---
+++ src/components/containers/hotelsContent/HotelsContent.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Breadcrumb } from 'antd';
+import separatorIcon from '@assets/svg/separator.svg';
+import { useAppSelector } from '@store/index';
+import formatDate from '@utils/helpers/formatDate';
+import Carousel from '../carousel/Carousel';
+import HotelsList from '../hotelsList/HotelsList';
+import cl from './HotelsContent.module.scss';
+
+const HotelsContent: React.FC = () => {
+  const currentData = useAppSelector((state) => state.currentData);
+
+  const readableDate = formatDate(currentData.date);
+
+  return (
+    <section className={cl.container}>
+      <div className={cl.breadcrumbContainer}>
+        <Breadcrumb className={cl.breadcrumb} separator="">
+          <>
+            <Breadcrumb.Item className={cl.breadcrumbItem}>
+              Отели
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator>
+              <img
+                src={separatorIcon}
+                className={cl.breadcrumbSeparator}
+                alt="breadcrumb-separator"
+              />
+            </Breadcrumb.Separator>
+          </>
+          <Breadcrumb.Item className={cl.breadcrumbItem}>
+            {currentData.city}
+          </Breadcrumb.Item>
+        </Breadcrumb>
+        <p className={cl.date}>{readableDate}</p>
+      </div>
+      <Carousel />
+
+      <div className={cl.hotelsListContainer}>
+        <p className={cl.favouritesText}>
+          {'Добавлено в избранное: '}
+          <span className={cl.counter}>{currentData.favourites.length}</span>
+          {'отеля '}
+        </p>
+        <HotelsList />
+      </div>
+    </section>
+  );
+};
+
+export default HotelsContent;
